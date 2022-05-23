cfdd7c3cc7f9056131834ca294bf1817b8909787
Juan Villela <juan@villela.co>
2022-05-23 16:36:13 -0400

    feat: Add table categories lists.

---
+++ src/categories.ts
@@ -0,0 +1,44 @@
+export const podcast = [
+  'crime',
+  'development',
+  'entertainment',
+  'gaming',
+  'journalism',
+  'science',
+  'technology',
+];
+
+export const youtube = [
+  'animals',
+  'art',
+  'beauty',
+  'comedy',
+  'development',
+  'education',
+  'engineering',
+  'fashion',
+  'film',
+  'finance',
+  'food',
+  'gaming',
+  'health',
+  'life',
+  'music',
+  'news',
+  'politics',
+  'productivity',
+  'science',
+  'technology',
+  'travel',
+  'tv',
+];
+
+export const website = [
+  'comic',
+  'development',
+  'entertainment',
+  'politics',
+  'science',
+  'technology',
+  'webtoons',
+];

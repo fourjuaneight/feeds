94e33c2a249e4399656c86cb69d646d92a69deba
Juan Villela <juan@villela.co>
2022-05-31 16:03:40 -0400

    feat: Check for existing feed before adding new.

--- src/hasura.ts
+++ src/hasura.ts
@@ -7,120 +7,6 @@ import {
 } from './typings.d';
 
 /**
- * Add feed entry to Hasura.
- * @function
- * @async
- *
- * @param {string} table
- * @param {Feed} item data to upload
- * @returns {Promise<string>}
- */
-export const addFeedItem = async (
-  table: string,
-  item: Feed
-): Promise<string> => {
-  const query = `
-    mutation {
-      insert_feeds_${table}_one(object: {
-        category: "${item.category}",
-        rss: "${item.rss}",
-        title: "${item.title}",
-        url: "${item.url}"
-       }) {
-        title
-      }
-    }
-  `;
-
-  try {
-    const request = await fetch(`${HASURA_ENDPOINT}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Hasura-Admin-Secret': `${HASURA_ADMIN_SECRET}`,
-      },
-      body: JSON.stringify({ query }),
-    });
-    const response: HasuraInsertResp | HasuraErrors = await request.json();
-
-    if (response.errors) {
-      const { errors } = response as HasuraErrors;
-
-      console.log('addFeedItem', errors);
-      throw `Adding record to Hasura - Feeds - ${table}: \n ${errors
-        .map(err => `${err.extensions.path}: ${err.message}`)
-        .join('\n')} \n ${query}`;
-    }
-
-    return (response as HasuraInsertResp)[`insert_feeds_${table}_one`].title;
-  } catch (error) {
-    console.log('addFeedItem', error);
-    throw `Adding record to Hasura - Feeds - ${table}: \n ${error}`;
-  }
-};
-
-/**
- * Update feed entry to Hasura.
- * @function
- * @async
- *
- * @param {string} table
- * @param {string} id item id
- * @param {Feed} item data to update
- * @returns {Promise<string>}
- */
-export const updateFeedItem = async (
-  table: string,
-  id: string,
-  item: Feed
-): Promise<string> => {
-  const query = `
-    mutation {
-      update_feeds_${table}(
-        where: {id: {_eq: "${id}"}},
-        _set: {
-          category: "${item.category}",
-          rss: "${item.rss}",
-          title: "${item.title}",
-          url: "${item.url}"
-        }
-      ) {
-        returning {
-          title
-        }
-      }
-    }
-  `;
-
-  try {
-    const request = await fetch(`${HASURA_ENDPOINT}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Hasura-Admin-Secret': `${HASURA_ADMIN_SECRET}`,
-      },
-      body: JSON.stringify({ query }),
-    });
-    const response: HasuraUpdateResp | HasuraErrors = await request.json();
-
-    if (response.errors) {
-      const { errors } = response as HasuraErrors;
-
-      console.log('updateFeedItem', errors);
-      throw `Updating record to Hasura - Feeds - ${table}: \n ${errors
-        .map(err => `${err.extensions.path}: ${err.message}`)
-        .join('\n')} \n ${query}`;
-    }
-
-    return (response as HasuraUpdateResp)[`update_feeds_${table}`].returning[0]
-      .title;
-  } catch (error) {
-    console.log('updateFeedItem', error);
-    throw `Updating record to Hasura - Feeds - ${table}: \n ${error}`;
-  }
-};
-
-/**
  * Get feed entries from Hasura.
  * @function
  * @async
@@ -218,5 +104,127 @@ export const searchFeedItems = async (
   } catch (error) {
     console.log('searchFeedItems', error);
     throw `Searching records from Hasura - Feeds - ${table}: \n ${error}`;
+  }
+};
+
+/**
+ * Add feed entry to Hasura.
+ * @function
+ * @async
+ *
+ * @param {string} table
+ * @param {Feed} item data to upload
+ * @returns {Promise<string>}
+ */
+export const addFeedItem = async (
+  table: string,
+  item: Feed
+): Promise<string> => {
+  const query = `
+    mutation {
+      insert_feeds_${table}_one(object: {
+        category: "${item.category}",
+        rss: "${item.rss}",
+        title: "${item.title}",
+        url: "${item.url}"
+       }) {
+        title
+      }
+    }
+  `;
+
+  try {
+    const existing = await searchFeedItems(table, item.title);
+
+    if (existing.length !== 0) {
+      console.log('addFeedItem', 'Feed already exists.');
+
+      throw `Adding record to Hasura - Feeds: Feed already exists.`;
+    }
+
+    const request = await fetch(`${HASURA_ENDPOINT}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Hasura-Admin-Secret': `${HASURA_ADMIN_SECRET}`,
+      },
+      body: JSON.stringify({ query }),
+    });
+    const response: HasuraInsertResp | HasuraErrors = await request.json();
+
+    if (response.errors) {
+      const { errors } = response as HasuraErrors;
+
+      console.log('addFeedItem', errors);
+      throw `Adding record to Hasura - Feeds - ${table}: \n ${errors
+        .map(err => `${err.extensions.path}: ${err.message}`)
+        .join('\n')} \n ${query}`;
+    }
+
+    return (response as HasuraInsertResp)[`insert_feeds_${table}_one`].title;
+  } catch (error) {
+    console.log('addFeedItem', error);
+    throw `Adding record to Hasura - Feeds - ${table}: \n ${error}`;
+  }
+};
+
+/**
+ * Update feed entry to Hasura.
+ * @function
+ * @async
+ *
+ * @param {string} table
+ * @param {string} id item id
+ * @param {Feed} item data to update
+ * @returns {Promise<string>}
+ */
+export const updateFeedItem = async (
+  table: string,
+  id: string,
+  item: Feed
+): Promise<string> => {
+  const query = `
+    mutation {
+      update_feeds_${table}(
+        where: {id: {_eq: "${id}"}},
+        _set: {
+          category: "${item.category}",
+          rss: "${item.rss}",
+          title: "${item.title}",
+          url: "${item.url}"
+        }
+      ) {
+        returning {
+          title
+        }
+      }
+    }
+  `;
+
+  try {
+    const request = await fetch(`${HASURA_ENDPOINT}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Hasura-Admin-Secret': `${HASURA_ADMIN_SECRET}`,
+      },
+      body: JSON.stringify({ query }),
+    });
+    const response: HasuraUpdateResp | HasuraErrors = await request.json();
+
+    if (response.errors) {
+      const { errors } = response as HasuraErrors;
+
+      console.log('updateFeedItem', errors);
+      throw `Updating record to Hasura - Feeds - ${table}: \n ${errors
+        .map(err => `${err.extensions.path}: ${err.message}`)
+        .join('\n')} \n ${query}`;
+    }
+
+    return (response as HasuraUpdateResp)[`update_feeds_${table}`].returning[0]
+      .title;
+  } catch (error) {
+    console.log('updateFeedItem', error);
+    throw `Updating record to Hasura - Feeds - ${table}: \n ${error}`;
   }
 };
